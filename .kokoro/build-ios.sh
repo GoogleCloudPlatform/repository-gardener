f2237a95fe24c4759221fb86825852468697c1f0
Ibrahim Ulukaya <ulukaya@google.com>
2018-07-16 17:05:43 -0400

    add a repository gardener for iOS Cocoapods dependencies using Podfile.lock. The actual build check is done via Travis once PR is submitted.

---
+++ .kokoro/build-ios.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# Build script for repositories with iOS Cocoapods dependencies.
+
+set -eo pipefail
+
+cd ${KOKORO_ARTIFACTS_DIR}/github/repository-gardener
+
+# Kokoro should set the following environment variables.
+# - DPEBOT_REPO
+# - DPEBOT_GIT_USER_NAME
+# - DPEBOT_GIT_USER_EMAIL="dpebot@google.com"
+# Kokoro exposes this as a file, but the scripts expect just a plain variable.
+export DPEBOT_GITHUB_TOKEN=$(cat ${KOKORO_GFILE_DIR}/${DPEBOT_GITHUB_TOKEN_FILE})
+
+chmod +x *.sh
+
+./clone-and-checkout.sh "${DPEBOT_REPO}"
+
+(
+cd repo-to-update
+../use-latest-deps-ios.sh "${DPEBOT_REPO}"
+)
