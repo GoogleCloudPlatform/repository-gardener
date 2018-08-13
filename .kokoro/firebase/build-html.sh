b5ec2f8f5124be2452f5448b242fe62762fe9063
Nicolas Garnier <nivco.las@gmail.com>
2018-08-13 22:12:47 +0200

    Adding support to update JS dependencies included in HTML using a <script> tag. (#64)
    Added support to update Firebase SDKs imported in HTML files.
    Also added Firebase repos configs

---
+++ .kokoro/firebase/build-html.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+# Build script for repositories with NodeJS dependencies.
+
+set -eo pipefail
+
+cd ${KOKORO_ARTIFACTS_DIR}/github/repository-gardener
+
+# Kokoro should set the following environment variables.
+# - DPEBOT_REPO
+# - DPEBOT_GIT_USER_NAME
+# - DPEBOT_GIT_USER_EMAIL="dpebot@google.com"
+# Kokoro exposes this as a file, but the scripts expect just a plain variable.
+export DPEBOT_GITHUB_TOKEN=$(cat ${KOKORO_GFILE_DIR}/${DPEBOT_GITHUB_TOKEN_FILE})
+
+chmod +x *.sh
+
+./clone-and-checkout.sh "${DPEBOT_REPO}"
+
+# Get latest version of the Firebase SDK on NPM
+FIREBASE_SDK_INFO=$(npm view firebase --json)
+FIREBASE_SDK_VER=$(node -e "console.log(${FIREBASE_SDK_INFO}['dist-tags'].latest)")
+
+# Get latest version of FirebaseUI on NPM
+FIREBASEUI_INFO=$(npm view firebaseui --json)
+FIREBASEUI_VER=$(node -e "console.log(${FIREBASEUI_INFO}['dist-tags'].latest)")
+
+(
+cd repo-to-update
+# Updating local/firebase hosing served Firebase SDK dependencies.
+../use-latest-deps-html.sh "firebase/[0-9]*\.[0-9]*\.[0-9]*/" "firebase/${FIREBASE_SDK_VER}/" "${DPEBOT_REPO}"
+# Updating CDN Firebase SDK dependencies.
+../use-latest-deps-html.sh "firebasejs/[0-9]*\.[0-9]*\.[0-9]*/" "firebasejs/${FIREBASE_SDK_VER}/" "${DPEBOT_REPO}"
+# Updating CDN FirebaseUI dependencies.
+../use-latest-deps-html.sh "firebaseui/[0-9]*\.[0-9]*\.[0-9]*/" "firebaseui/${FIREBASEUI_VER}/" "${DPEBOT_REPO}"
+)
