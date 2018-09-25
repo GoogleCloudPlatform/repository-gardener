53bf15914f1132ea47e1e49e2c770fb81bdd6ee4
Sam Stern <samstern@google.com>
2018-09-25 15:09:12 -0700

    Updates for Android 28

--- fix_android_dependencies.py
+++ fix_android_dependencies.py
@@ -4,9 +4,9 @@ import re
 
 # These will have to be updated manually over time, there's not an
 # easy way to determine the latest version.
-COMPILE_SDK_VERSION = 27
-TARGET_SDK_VERSION = 27
-BUILD_TOOLS_VERSION = '27.0.3'
+COMPILE_SDK_VERSION = 28
+TARGET_SDK_VERSION = 28
+BUILD_TOOLS_VERSION = '28.0.3'
 
 COMPILE_SDK_RE = r'compileSdkVersion[\s][\w]+'
 TARGET_SDK_RE = r'targetSdkVersion[\s][\w]+'
