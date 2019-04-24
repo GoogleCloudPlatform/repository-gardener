c53026d20b19f1ece59bd567687186301997c2b7
Brent Shaffer <betterbrent@google.com>
2019-04-24 14:30:02 -0700

    adds trampoline for php

--- .kokoro/build-php.sh
+++ .kokoro/build-php.sh
@@ -13,15 +13,7 @@ cd ${KOKORO_ARTIFACTS_DIR}/github/repository-gardener
 export DPEBOT_GITHUB_TOKEN=$(cat ${KOKORO_GFILE_DIR}/${DPEBOT_GITHUB_TOKEN_FILE})
 
 # Install jq
-sudo apt-get install -y jq
-
-# Install composer
-php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-php -r "if (hash_file('sha384', 'composer-setup.php') === '48e3236262b34d30969dca3c37281b3b4bbe3221bda826ac6a9a62d6444cdb0dcd0615698a5cbe587c3f0fe57a54d8f5') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
-php composer-setup.php
-php -r "unlink('composer-setup.php');"
-chmod +x composer.phar
-mv composer.phar /usr/local/bin/composer
+apt-get install -y jq
 
 chmod +x *.sh
 
