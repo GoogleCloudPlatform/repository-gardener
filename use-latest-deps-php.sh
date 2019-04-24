94e44bb992b71e0b214097309e7d2931759f4ea4
Brent Shaffer <betterbrent@google.com>
2019-04-24 16:12:13 -0700

    uses bash arrays

--- use-latest-deps-php.sh
+++ use-latest-deps-php.sh
@@ -69,21 +69,26 @@ for DIR in $directories; do
   pushd "$DIR"
   composer install --ignore-platform-reqs --no-dev
 
-  OUTDATED=$(echo \
+  updatePackages=()
+  outdatedPackages=$(echo \
     "$(composer outdated 'google/*' --direct --format=json | jq '.installed' 2>/dev/null) $(composer outdated 'firebase/*' --direct --format=json | jq '.installed' 2>/dev/null)" \
     | jq -s add)
 
-  if [[ "$OUTDATED" != "null" ]] && [[ "$OUTDATED" != "[]" ]] ; then
-    count=$(echo "$OUTDATED" | jq length)
+  if [[ "$outdatedPackages" != "null" ]] && [[ "$outdatedPackages" != "[]" ]] ; then
+    count=$(echo "$outdatedPackages" | jq length)
 
     for (( i = 0; i < count; i++ ))
     do
-      name=$(echo "$OUTDATED" | jq -r --arg i "$i" '.[$i | tonumber].name')
-      version=$(echo "$OUTDATED" | jq -r --arg i "$i" '.[$i | tonumber].latest' | sed -e 's/^v//')
+      name=$(echo "$outdatedPackages" | jq -r --arg i "$i" '.[$i | tonumber].name')
+      version=$(echo "$outdatedPackages" | jq -r --arg i "$i" '.[$i | tonumber].latest' | sed -e 's/^v//')
       if [[ "${version:0:4}" != dev- ]]; then
-        composer require --ignore-platform-reqs --update-no-dev --update-with-dependencies "$name:^$version"
+        updatePackages+=( "$name:^$version" )
       fi
     done
+
+    if [ ${#updatePackages[@]} -gt 0 ]; then
+      composer require --ignore-platform-reqs --update-no-dev --update-with-dependencies "${updatePackages[@]}"
+    fi
   fi
 
   popd
