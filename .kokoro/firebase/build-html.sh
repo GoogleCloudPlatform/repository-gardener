1e26aee9b50d2e47298ffa45b6fff3e08d606929
Tom Lanyon <tomlanyon@google.com>
2020-10-07 17:51:29 +1100

    Update build-html.sh
    `npm view` has built-in support for picking out individual fields; switch to this instead of relying on exporting then parsing json for extra safety.

--- .kokoro/firebase/build-html.sh
+++ .kokoro/firebase/build-html.sh
@@ -27,10 +27,10 @@ sudo apt-get install -y jq
 sudo npm install -g npm@latest
 
 # Get latest version of the Firebase SDK on NPM
-FIREBASE_SDK_VER=$(npm view firebase --json | jq -r '.["dist-tags"].latest')
+FIREBASE_SDK_VER=$(npm view firebase@latest version')
 
 # Get latest version of FirebaseUI on NPM
-FIREBASEUI_VER=$(npm view firebaseui --json | jq -r '.["dist-tags"].latest')
+FIREBASEUI_VER=$(npm view firebaseui@latest version')
 
 (
 cd repo-to-update
