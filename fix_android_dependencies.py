71b723561a26e6703dd470054fd9456de548e9d2
Rosário Pereira Fernandes <rosariofernandes51@gmail.com>
2022-01-10 20:10:20 +0200

    refactor: update Android SDK to API level 31 (#151)

--- fix_android_dependencies.py
+++ fix_android_dependencies.py
@@ -4,9 +4,9 @@ import re
 
 # These will have to be updated manually over time, there's not an
 # easy way to determine the latest version.
-COMPILE_SDK_VERSION = 30
-TARGET_SDK_VERSION = 30
-BUILD_TOOLS_VERSION = '30.0.0'
+COMPILE_SDK_VERSION = 31
+TARGET_SDK_VERSION = 31
+BUILD_TOOLS_VERSION = '30.0.2'
 
 COMPILE_SDK_RE = r'compileSdkVersion[\s][\w]+'
 TARGET_SDK_RE = r'targetSdkVersion[\s][\w]+'
