c5ba69e07907e223e38df023368f2f6b5efb5dd9
Rosário Pereira Fernandes <rosariofernandes51@gmail.com>
2022-08-13 14:00:33 +0100

    update Android SDK to API level 33

--- fix_android_dependencies.py
+++ fix_android_dependencies.py
@@ -4,8 +4,8 @@ import re
 
 # These will have to be updated manually over time, there's not an
 # easy way to determine the latest version.
-COMPILE_SDK_VERSION = 32
-TARGET_SDK_VERSION = 32
+COMPILE_SDK_VERSION = 33
+TARGET_SDK_VERSION = 33
 BUILD_TOOLS_VERSION = '30.0.2'
 
 COMPILE_SDK_RE = r'compileSdkVersion[\s][\w]+'
