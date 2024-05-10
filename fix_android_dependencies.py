0682620fc5f073947d10364e02bfa26b8a359763
rosariopf <rosariopf@google.com>
2024-05-10 02:21:30 +0100

    style: improve code style

--- fix_android_dependencies.py
+++ fix_android_dependencies.py
@@ -18,6 +18,7 @@ BUILD_TOOLS_RE = r'buildTools(?:Version)?\s*=?\s*[\'\"\w\.]+'
 # $ ./gradlew dependencyUpdates -Drevision=release -DoutputFormatter=json
 RELATIVE_PATH_TO_JSON_REPORT = 'build/dependencyUpdates/report.json'
 
+
 def find_gradle_files():
     """Finds all build.gradle(.kts) files, recursively."""
     gradle_files = []
@@ -28,19 +29,21 @@ def find_gradle_files():
 
     return gradle_files
 
+
 def get_android_replacements():
     """Gets a dictionary of all android-specific replacements to be made."""
     replacements = {}
 
-    compileSdk = f"compileSdk = {COMPILE_SDK_VERSION}"
-    targetSdk = f"targetSdk = {TARGET_SDK_VERSION}"
-    buildToolsVersion = f"buildToolsVersion = \"{BUILD_TOOLS_VERSION}\""
+    compile_sdk = f"compileSdk = {COMPILE_SDK_VERSION}"
+    target_sdk = f"targetSdk = {TARGET_SDK_VERSION}"
+    build_tools_version = f"buildToolsVersion = \"{BUILD_TOOLS_VERSION}\""
 
-    replacements[COMPILE_SDK_RE] = compileSdk
-    replacements[TARGET_SDK_RE] = targetSdk
-    replacements[BUILD_TOOLS_RE] = buildToolsVersion
+    replacements[COMPILE_SDK_RE] = compile_sdk
+    replacements[TARGET_SDK_RE] = target_sdk
+    replacements[BUILD_TOOLS_RE] = build_tools_version
 
     return replacements
+
 
 def is_major_update(old_version, new_version):
     """Compares version strings to see if it's a major update."""
@@ -48,6 +51,7 @@ def is_major_update(old_version, new_version):
     new_major = new_version.split('.')[0]
 
     return old_major != new_major
+
 
 def get_dep_replacements(json_file):
     """Gets a dictionary of all dependency replacements to be made."""
@@ -69,11 +73,12 @@ def get_dep_replacements(json_file):
             replacements[curr_dep] = new_dep
 
             # For the plugins block
-            curr_plugin = f'\("{group}"\) version "{curr_version}"'
+            curr_plugin = f'("{group}") version "{curr_version}"'
             new_plugin = f'("{group}") version "{new_version}"'
             replacements[curr_plugin] = new_plugin
 
     return replacements
+
 
 def update_project(project_path):
     """Runs through all build.gradle(.kts) files and performs replacements for individual android project."""
@@ -82,13 +87,13 @@ def update_project(project_path):
     replacements.update(get_dep_replacements(project_path))
 
     # Print all updates found
-    print ("Dependency updates:")
+    print("Dependency updates:")
     for (k, v) in iter(replacements.items()):
-        print (f"{k} --> {v}")
+        print(f"{k} --> {v}")
 
     # Iterate through each file and replace it
     for gradle_file in find_gradle_files():
-        print (f"Updating dependencies for: {gradle_file}")
+        print(f"Updating dependencies for: {gradle_file}")
 
         new_data = ''
         with open(gradle_file, 'r') as f:
@@ -101,35 +106,38 @@ def update_project(project_path):
         with open(gradle_file, 'w') as f:
             f.write(new_data)
 
+
 def update_all():
     """Runs through all build.gradle files and performs replacements."""
 
     project_root = os.getcwd()
-    print (f"Repo root: {project_root}")
+    print(f"Repo root: {project_root}")
 
     top_level_report = os.path.join(project_root, RELATIVE_PATH_TO_JSON_REPORT)
 
     if os.path.exists(top_level_report):
-        print ("Update dependencies via top-level report")
+        print("Update dependencies via top-level report")
         update_project(top_level_report)
     else:
-        print ("Update dependencies via child-level report(s)")
+        print("Update dependencies via child-level report(s)")
         first_level_subdirectories = get_immediate_subdirectories(project_root)
-        print (f"List of subdirectories: {first_level_subdirectories}")
+        print(f"List of subdirectories: {first_level_subdirectories}")
 
         for subdirectory in first_level_subdirectories:
-            print (f"subdirectory: {subdirectory}")
+            print(f"subdirectory: {subdirectory}")
             subdirectory_report = os.path.join(project_root, subdirectory, RELATIVE_PATH_TO_JSON_REPORT)
 
             if os.path.exists(subdirectory_report):
-                print ("\tUpdate dependencies in subdirectory")
+                print("\tUpdate dependencies in subdirectory")
                 update_project(subdirectory_report)
             else:
-                print ("\tNo report in subdirectory")
+                print("\tNo report in subdirectory")
+
 
 def get_immediate_subdirectories(directory):
     return [name for name in os.listdir(directory)
             if os.path.isdir(os.path.join(directory, name)) and not name.startswith('.')]
+
 
 if __name__ == '__main__':
     update_all()
