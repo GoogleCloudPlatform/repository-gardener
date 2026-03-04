c866b7781ee1dc9ccd18a75dcd889be72dc140f0
Rosario Fernandes <rosariopf@google.com>
2026-03-04 23:43:46 +0000

    Fix exit code propagation in use-latest-deps-android.sh
    This change ensures that failures in either the Gradle dependencyUpdates task or the  fix_android_dependencies.py script are correctly captured and cause the shell script to exit with a non-zero code. Previously, some failures were being ignored, leading to false-positive build results.

--- use-latest-deps-android.sh
+++ use-latest-deps-android.sh
@@ -63,11 +63,12 @@ update_dependencies () {
 
     # Run Android fixer script
     python3 "${dir}/fix_android_dependencies.py"
+    python_exit_code="$?"
 
     # Remove the virtualenv
     rm -rf env
 
-    return 0
+    return $python_exit_code
   else
     # Return invalid arguments exit code.
     return 128
@@ -133,7 +134,13 @@ if [ $generate_dependencies_report_exit_code -eq 127 ]; then
   for child_dir in "${child_dirs[@]}"
     do
       generate_dependencies_report "${DIR}/${child_dir}"
+      child_exit_code="$?"
+      if [ $child_exit_code -ne 0 ] && [ $child_exit_code -ne 127 ]; then
+        exit $child_exit_code
+      fi
     done
+elif [ $generate_dependencies_report_exit_code -ne 0 ]; then
+  exit $generate_dependencies_report_exit_code
 fi
 
 set -e
